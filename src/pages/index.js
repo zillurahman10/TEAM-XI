8ffeba260c2ee0b4c21f6fd39b4dee0c920eb4a6
zillurahman10 <zillur.hero40@gmail.com>
2023-08-17 22:10:10 +0600

    RootLayout added

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,26 @@
+import Image from 'next/image'
 import logo from '../assets/logo.PNG'
+import RootLayout from '@/components/Layout/RootLayout';
 
-export default function Home() {
+function Home() {
   return (
-    <div>
+    <div className='flex justify-between mx-24 my-8'>
       <div>
-        <img className='w-[500px]' src={logo} alt=""></img>
+        <Image src={logo} width={400} height={500}></Image>
       </div>
       <div>
-        <h1>Team XI</h1>
+        <h1 className='text-left'>Team XI</h1>
       </div>
     </div>
+  )
+}
+
+export default Home;
+
+Home.getLayout = function getLayout(page) {
+  return (
+    <RootLayout>
+      {page}
+    </RootLayout>
   )
 }
